d1b20ba979b0a4e56b6f9c27177eca55fd7ff508
hangyeol-lee <79792506+hangyeol-lee@users.noreply.github.com>
2021-03-21 23:40:43 +0900

    First Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Pandas-Excel Tutorial
+
+![sample](Figure_1.png)
---
+++ pandas_tutorial.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+excel_file = pd.ExcelFile('movies.xls')
+
+movies_sheet1 = pd.read_excel(excel_file, sheet_name=0, index_col=0)
+movies_sheet1.head()
+
+movies_sheet2 = pd.read_excel(excel_file, sheet_name=1, index_col=0)
+movies_sheet2.head()
+
+movies_sheet3 = pd.read_excel(excel_file, sheet_name=2, index_col=0)
+movies_sheet3.head()
+
+movies = pd.concat([movies_sheet1, movies_sheet2, movies_sheet3])
+
+movies['IMDB Score'].plot(kind="hist")
+plt.show()
